45794043b024a55bcb60c7f986007d261a64b3af
Nate Berens <nrberens@gmail.com>
2015-03-10 21:12:10 -0400

    moved particle system prefab to prevent stray particles

---
+++ Assets/Scripts/Effects/DeathEffect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathEffect : MonoBehaviour {
+
+	public ParticleSystem ps;
+	public float effectTime;
+
+	// Use this for initialization
+	void Start () {
+		ps = GetComponent<ParticleSystem>();
+		StartCoroutine (PlayEffect());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator PlayEffect() {
+		float startTime;
+
+		startTime = Time.time;
+
+		ps.Play ();
+		yield return new WaitForSeconds(1.5f);
+		ps.Stop ();
+		Destroy (gameObject);
+	}
+}

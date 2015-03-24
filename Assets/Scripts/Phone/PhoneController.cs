340eba3390a174bf84edc95abbac3248d830383e
Nate Berens <nrberens@gmail.com>
2015-03-23 20:13:12 -0400

    set up basic phone class and gameobject

---
+++ Assets/Scripts/Phone/PhoneController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneController : MonoBehaviour, ITurnBased {
+    public static PhoneController pc;
+
+    public Turn CurrentTurn { get; set; }
+    public bool acting { get; set; }
+
+    public Transform phoneMesh;
+
+    public int turnsBetweenSpawn;
+    public int turnsUntilSpawn;
+
+    void Awake() {
+        if (pc == null) {
+            DontDestroyOnLoad(gameObject);
+            pc = this;
+        } else if (pc != this) {
+            Destroy(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        turnsUntilSpawn = turnsBetweenSpawn;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

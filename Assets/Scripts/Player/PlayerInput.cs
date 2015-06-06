6991f0d56cb54ef9a7bc1ab5537db66cafa01388
Nate Berens <nrberens@gmail.com>
2015-06-05 21:58:17 -0400

    movablenodes now stay tagged when the mouse hovers over them

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -6,7 +6,7 @@ public class PlayerInput : MonoBehaviour
 {
 
 	private float moveX, moveZ;
-	public bool allowInput, playerSelected;
+	public bool allowInput, playerSelected, nodesTagged;
 	public Transform selectedBlock;
 
 	//public Vector3 crosshairPos;
@@ -29,106 +29,110 @@ public class PlayerInput : MonoBehaviour
 			//INPUT PHASE 
 			//TODO track time between inputs, only allow input every half second or so
 			if (allowInput && !PlayerController.pc.acting) {
-							PlayerController.pc.Mover.TagMovableNodes ();
-							PlayerController.pc.Shooter.TagShootableEnemies ();
+				if(!nodesTagged) {
+					PlayerController.pc.Mover.TagMovableNodes ();
+					PlayerController.pc.Shooter.TagShootableEnemies ();
+					nodesTagged = true;
+				}
 
 
-						for (int i = 0; i < PlayerController.pc.Shooter.shootableEnemies.Count; i++) {
-							EnemyController enemy = PlayerController.pc.Shooter.shootableEnemies [i];
-							ShootableNode shootableNode = enemy.Mover.currentNode.transform.parent.GetComponentInChildren<ShootableNode> ();
-							if (!PlayerController.pc.Shooter.CheckMeleeRange (enemy.transform))
-								shootableNode.currentState = ShootableNode.NodeState.ShootableUnselected;
-						}
+				for (int i = 0; i < PlayerController.pc.Shooter.shootableEnemies.Count; i++) {
+					EnemyController enemy = PlayerController.pc.Shooter.shootableEnemies [i];
+					ShootableNode shootableNode = enemy.Mover.currentNode.transform.parent.GetComponentInChildren<ShootableNode> ();
+					if (!PlayerController.pc.Shooter.CheckMeleeRange (enemy.transform))
+						shootableNode.currentState = ShootableNode.NodeState.ShootableUnselected;
+				}
 
-						//drag to node or enemy
-						//create layerMask = test enemies and world layer
-						int enemyLayer = 10;
-						int worldLayer = 11;
-						int layerMask = 1 << enemyLayer | 1 << worldLayer;
-						Transform target = GetTargetOfClick (layerMask);
-						if (target != null) {
-							if (target.tag == "WorldBlock") {
-								//no selected block, select new block
-								if (selectedBlock == null) {
-									MoveNode node = target.GetComponentInChildren<MoveNode> ();
-									if (node.movable) {
-										selectedBlock = target;
-										MovableNode nodeController = target.GetComponentInChildren<MovableNode> ();
-										nodeController.currentState = MovableNode.NodeState.MovableSelected;
-									}
-									//mouse over new block
-								} else if (target != selectedBlock) {
-									//unselect previous block
-									MovableNode previousNode = selectedBlock.GetComponentInChildren<MovableNode> ();
-									previousNode.currentState = MovableNode.NodeState.MovableUnselected;
-									selectedBlock = null;
-									//select new block
-									MoveNode node = target.GetComponentInChildren<MoveNode> ();
-									if (node.movable) {
-										selectedBlock = target;
-										MovableNode nodeController = target.GetComponentInChildren<MovableNode> ();
-										nodeController.currentState = MovableNode.NodeState.MovableSelected;
-									}
-								}
-							} else {
-								//not a block, unselect previous block
-								//MovableNode previousNode = target.GetComponentInChildren<MovableNode>();
-								//previousNode.currentState = MovableNode.NodeState.MovableUnselected;
-
-								if (target.tag == "Enemy")  {
-									if (PlayerController.pc.Shooter.CheckMeleeRange (target)) {
-										EnemyController enemy = target.GetComponent<EnemyController> ();
-											MovableNode node =
-												enemy.Mover.currentNode.transform.parent
-													.GetComponentInChildren<MovableNode> ();
-											node.currentState = MovableNode.NodeState.MeleeableSelected;
-									} else {
-										EnemyController enemy = target.GetComponent<EnemyController> ();
-										if (enemy.shootable) {
-											ShootableNode node =
-	                                            enemy.Mover.currentNode.transform.parent
-	                                                .GetComponentInChildren<ShootableNode> ();
-											node.currentState = ShootableNode.NodeState.ShootableSelected;
-										}
-									}
-								}
+				//mouse over node or enemy
+				//create layerMask = test enemies and world layer
+				int enemyLayer = 10;
+				int worldLayer = 11;
+				int layerMask = 1 << enemyLayer | 1 << worldLayer;
+				Transform target = GetTargetOfClick (layerMask);
+				if (target != null) {
+					if (target.tag == "WorldBlock") {
+						//no selected block, select new block
+						if (selectedBlock == null) {
+							MoveNode node = target.GetComponentInChildren<MoveNode> ();
+							if (node.movable) {
+								selectedBlock = target;
+								MovableNode nodeController = target.GetComponentInChildren<MovableNode> ();
+								nodeController.currentState = MovableNode.NodeState.MovableSelected;
 							}
-						} else if (target == null) {
+							//mouse over new block
+						} else if (target != selectedBlock) {
+							//unselect previous block
 							MovableNode previousNode = selectedBlock.GetComponentInChildren<MovableNode> ();
 							previousNode.currentState = MovableNode.NodeState.MovableUnselected;
 							selectedBlock = null;
+							//select new block
+							MoveNode node = target.GetComponentInChildren<MoveNode> ();
+							if (node.movable) {
+								selectedBlock = target;
+								MovableNode nodeController = target.GetComponentInChildren<MovableNode> ();
+								nodeController.currentState = MovableNode.NodeState.MovableSelected;
+							}
 						}
-						
-					 if (Input.GetMouseButtonDown (0)) {
-						selectedBlock = null;
-						PlayerController.pc.Mover.UnTagMovableNodes ();
-						PlayerController.pc.Shooter.UnTagShootableEnemies ();
+					} else {
+						//not a block, unselect previous block
+						//MovableNode previousNode = target.GetComponentInChildren<MovableNode>();
+						//previousNode.currentState = MovableNode.NodeState.MovableUnselected;
 
-						// create layerMask = test enemies and world layer
-						enemyLayer = 10;
-						worldLayer = 11;
-						layerMask = 1 << enemyLayer | 1 << worldLayer;
-						target = GetTargetOfClick (layerMask);
-						Debug.Log ("MouseUp - Target = " + target);
-						if (target != null) {
-							if (target.tag == "Enemy" && PlayerController.pc.Shooter.CheckValidTarget(target)) {
-								PlayerController.pc.acting = true;
-								allowInput = false;
-								PlayerController.pc.Shooter.BeginShot (target);
-							} else if (target.tag == "WorldBlock") {
-								Direction? dir = PlayerController.pc.Mover.GetTargetDirection (target.Find ("MoveNode").GetComponent<MoveNode> ());
-								if (dir != null) {
-									PlayerController.pc.acting = true;
-									allowInput = false;
-									Direction moveDir = (Direction)dir;
-									PlayerController.pc.Mover.Move (moveDir, 1);
+						if (target.tag == "Enemy") {
+							if (PlayerController.pc.Shooter.CheckMeleeRange (target)) {
+								EnemyController enemy = target.GetComponent<EnemyController> ();
+								MovableNode node =
+												enemy.Mover.currentNode.transform.parent
+													.GetComponentInChildren<MovableNode> ();
+								node.currentState = MovableNode.NodeState.MeleeableSelected;
+							} else {
+								EnemyController enemy = target.GetComponent<EnemyController> ();
+								if (enemy.shootable) {
+									ShootableNode node =
+	                                            enemy.Mover.currentNode.transform.parent
+	                                                .GetComponentInChildren<ShootableNode> ();
+									node.currentState = ShootableNode.NodeState.ShootableSelected;
 								}
-
 							}
 						}
 					}
+				} else if (target == null) {
+					MovableNode previousNode = selectedBlock.GetComponentInChildren<MovableNode> ();
+					previousNode.currentState = MovableNode.NodeState.MovableUnselected;
+					selectedBlock = null;
+				}
+						
+				if (Input.GetMouseButtonDown (0)) {
+					selectedBlock = null;
+					PlayerController.pc.Mover.UnTagMovableNodes ();
+					PlayerController.pc.Shooter.UnTagShootableEnemies ();
+					nodesTagged = false;
+
+					// create layerMask = test enemies and world layer
+					enemyLayer = 10;
+					worldLayer = 11;
+					layerMask = 1 << enemyLayer | 1 << worldLayer;
+					target = GetTargetOfClick (layerMask);
+					Debug.Log ("MouseUp - Target = " + target);
+					if (target != null) {
+						if (target.tag == "Enemy" && PlayerController.pc.Shooter.CheckValidTarget (target)) {
+							PlayerController.pc.acting = true;
+							allowInput = false;
+							PlayerController.pc.Shooter.BeginShot (target);
+						} else if (target.tag == "WorldBlock") {
+							Direction? dir = PlayerController.pc.Mover.GetTargetDirection (target.Find ("MoveNode").GetComponent<MoveNode> ());
+							if (dir != null) {
+								PlayerController.pc.acting = true;
+								allowInput = false;
+								Direction moveDir = (Direction)dir;
+								PlayerController.pc.Mover.Move (moveDir, 1);
+							}
+
+						}
+					}
+				}
 				
-                //END OF TURN
+				//END OF TURN
 			} else if (!allowInput && !PlayerController.pc.acting) {
 				PlayerController.pc.EndPhase ();
 			}
